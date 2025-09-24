db6f483efcf1173d24401e02fa0511faa01ddadd
cooljiit <73384678+cooljiit@users.noreply.github.com>
2025-09-24 21:30:13 +0530

    Added CircuitOpenException Advice

--- src/main/java/pl/cleankod/exchange/entrypoint/ExceptionHandlerAdvice.java
+++ src/main/java/pl/cleankod/exchange/entrypoint/ExceptionHandlerAdvice.java
@@ -1,5 +1,6 @@
 package pl.cleankod.exchange.entrypoint;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,5 +16,12 @@ public class ExceptionHandlerAdvice {
     })
     protected ResponseEntity<ApiError> handleBadRequest(CurrencyConversionException ex) {
         return ResponseEntity.badRequest().body(new ApiError(ex.getMessage()));
+    }
+
+    @ExceptionHandler({
+            pl.cleankod.exchange.core.usecase.CircuitOpenException.class
+    })
+    protected ResponseEntity<ApiError> handleServiceUnavailable(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new ApiError(ex.getMessage()));
     }
 }
